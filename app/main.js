407ed5c300e2ea9afb13db291b2c466da5e4e0fc
eduvinagre <eduvinagre@gmail.com>
2023-05-29 15:52:18 -0300

    innerHTML commit

--- app/main.js
+++ app/main.js
@@ -3,8 +3,31 @@ const APIEndpoint =
   "https://guilhermeonrails.github.io/casadocodigo/livros.json";
 getBooks();
 
+const insertBooksByID = document.getElementById("livros");
+
 async function getBooks() {
   const res = await fetch(APIEndpoint);
   books = await res.json();
-  console.log(books);
+  console.table(books);
+
+  showBooks(books);
+}
+
+function showBooks(booksTable) {
+  booksTable.forEach((livro) => {
+    insertBooksByID.innerHTML += `
+        <div class="livro">
+      <img class="livro__imagens" src="imagens/Cangaceiro-Javascript.png" alt="Capa do livro Cangaceiro JavaScript" />
+      <h2 class="livro__titulo">
+        Cangaceiro JavaScript:
+        Uma aventura no sertão da programação
+      </h2>
+      <p class="livro__descricao">Flávio Almeida</p>
+      <p class="livro__preco" id="preco">R$29,90</p>
+      <div class="tags">
+        <span class="tag">Front-end</span>
+      </div>
+    </div>
+    `;
+  });
 }
