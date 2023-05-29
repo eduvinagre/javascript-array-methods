b9dca196b1fa0a4d92f91f9da168a240b11611cb
eduvinagre <eduvinagre@gmail.com>
2023-05-29 15:18:55 -0300

    api update

---
+++ app/main.js
@@ -0,0 +1,10 @@
+let books = [];
+const APIEndpoint =
+  "https://guilhermeonrails.github.io/casadocodigo/livros.json";
+getBooks();
+
+async function getBooks() {
+  const res = await fetch(APIEndpoint);
+  books = await res.json();
+  console.log(books);
+}
